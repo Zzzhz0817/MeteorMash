f91e99b55257a1a5a520e2daef14f7c838f3380d
Playstarstudio <charles@playstarstudio.com>
2024-11-12 21:13:04 -0500

    New State

---
+++ Assets/Scripts/StateMachine/BaseStateBase.cs
@@ -0,0 +1,21 @@
+﻿public abstract class BaseStateBase
+{
+    public virtual void FixedUpdate()
+    {
+
+    }
+    public virtual void OnEnter()
+    {
+
+    }
+
+
+    public virtual void OnExit()
+    {
+
+    }
+    public virtual void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/StateMachine/IPredicate.cs
@@ -0,0 +1,10 @@
+﻿public interface IPredicate
+{
+    bool Evaluate();
+}
+
+public class FuncPredicate : IPredicate
+{
+    private FuncPredicate<bool> func;
+
+}
