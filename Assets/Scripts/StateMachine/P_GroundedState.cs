574d7460c9fdc014075b87513118bfcbffea7c81
Playstarstudio <charles@playstarstudio.com>
2024-11-19 17:54:10 -0500

    Update Anim Tree

--- Assets/Scripts/StateMachine/P_GroundedState.cs
+++ Assets/Scripts/StateMachine/P_GroundedState.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class P_GroundedState : P_State
@@ -32,7 +34,7 @@ public class P_GroundedState : P_State
 
         }
         */
-        
+
         // Movement
         float horizontalInput = Input.GetAxis("Horizontal"); // A/D keys
         float verticalInput = Input.GetAxis("Vertical");     // W/S keys
@@ -57,6 +59,8 @@ public class P_GroundedState : P_State
             player.transform.Rotate(0f, 0f, rollInput * player.rotationSpeed * 0.3f * Time.deltaTime, Space.Self);
         }
 
+        AnimTracker(player);
+
         // Check for aiming
         Vector3 toCenter = player.groundedObject.position - player.transform.position;
         float angle = Vector3.Angle(player.transform.forward, toCenter);
@@ -79,11 +83,67 @@ public class P_GroundedState : P_State
         }
     }
 
+    private void AnimTracker(P_StateManager player)
+    {
+        int h = Input.GetKey(KeyCode.A) ? -1 : Input.GetKey (KeyCode.D) ? 1 : 0;
+        int v = Input.GetKey(KeyCode.S) ? -1 : Input.GetKey(KeyCode.W) ? 1 : 0; ;
+        string horizontal = h.ToString();
+        string vertical = v.ToString();
+        string c = horizontal + " " + vertical;
+        Debug.Log(c);
+        switch (c)
+        {
+            case "-1 -1":
+                ResetAnims(player);
+                player.anim.SetBool("Move-Down-Left", true);
+                break;
+            case "-1 0":
+                ResetAnims(player);
+                player.anim.SetBool("Move-Left", true);
+                break;
+            case "-1 1":
+                ResetAnims(player);
+                player.anim.SetBool("Move-Up-Left", true);
+                break;
+            case "0 -1":
+                ResetAnims(player);
+                player.anim.SetBool("Move-Down", true);
+                break;
+            case "0 0":
+                ResetAnims(player);
+                player.anim.SetBool("Crawl-Idle", true);
+                break;
+            case "0 1":
+                ResetAnims(player);
+                player.anim.SetBool("Move-Up", true);
+                break;
+            case "1 -1":
+                ResetAnims(player);
+                player.anim.SetBool("Move-Down-Right", true);
+                break;
+            case "1 0":
+                ResetAnims(player);
+                player.anim.SetBool("Move-Right", true);
+                break;
+            case "1 1":
+                ResetAnims(player);
+                player.anim.SetBool("Move-Up-Right", true);
+                break;
+            default:
+                ResetAnims(player);
+                player.anim.SetBool("Crawl-Idle", true);
+                break;
+        }
+        
+
+        
+    }
     public override void ExitState(P_StateManager player)
     {
         player.transform.SetParent(null);
         player.rb.isKinematic = false;
         player.anim.SetBool("Crawl-Idle", false);
+        ResetAnims(player);
     }
 
     private void MoveAlongSphere(P_StateManager player, float horizontalInput, float verticalInput)
@@ -102,7 +162,19 @@ public class P_GroundedState : P_State
 
         AlignWithSurfaceNormal(player);
     }
+    private void ResetAnims(P_StateManager player)
+    {
+        player.anim.SetBool("Move-Up", false);
+        player.anim.SetBool("Move-Up-Right", false);
+        player.anim.SetBool("Move-Right", false);
+        player.anim.SetBool("Move-Down-Right", false);
+        player.anim.SetBool("Move-Down", false);
+        player.anim.SetBool("Move-Down-Left", false);
+        player.anim.SetBool("Move-Left", false);
+        player.anim.SetBool("Move-Up-Left", false);
+        player.anim.SetBool("Crawl-Idle", false);
 
+    }
     private void AlignWithSurfaceNormal(P_StateManager player)
     {
         Vector3 normal = (player.transform.position - player.groundedObject.position).normalized;
